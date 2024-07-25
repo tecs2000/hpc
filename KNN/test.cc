6fa087a0acad172476178b765da4413160a96f22
Thayná Santos <tecs@cin.ufpe.br>
2024-07-25 14:59:18 -0300

    final serial version of knn

---
+++ KNN/test.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string a = "a";
+    string b = "a\n";
+
+    if (a != b){
+        cout << "diff" << endl;
+        cout << a << endl;
+    }
+    else
+        cout << "equal";
+    return 0;
+}
